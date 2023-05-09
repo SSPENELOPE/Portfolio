4e108e8c6376057e50646a36496e6e8c8149a75e
SSPENELOPE <typoepping@gmail.com>
2023-05-09 11:22:40 -0400

    updated portfolio to match current credentials and experience

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -6,7 +6,7 @@ function Aboutme() {
             <div className="selfPortrait">
             </div>
             <div className="biography">
-                <p>I am a 7 year Army veteran. I've been to 17 different countries and absolutely love to travel. I recently gradutated the University of Minnesota Coding Bootcamp. I specialize in React.js and the MERN Stack. I am continuing my studies in web/software development and learning Java! Currently open to work</p>
+                <p>I am a 7 year Army veteran and travel enthusiast. I've been to 17 different countries and my favorite so far has been Finland! My current tech stack has migrated to React, ASP.NET and C#. I am currently developing and publishing applications on Microsoft Azure. I pride myself in being honest, maintaining my integrity, and being a great team player. I am currently open to work!</p>
             </div>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,19 +7,20 @@ function Projects() {
         <div className="projectWrapper">
 
                 <div className="container">
+                    <a href="https://starweather.azurewebsites.net/" target="_blank" rel="noreferrer" className="work-box beard">
+                        <h3>React/C#/ASP.NET</h3>
+                        <a href="https://github.com/SSPENELOPE/react-weatherapp" target="_blank" rel="noreferrer" className="abc"><FontAwesomeIcon icon={faGithub} /></a>
+                    </a>
+
                     <a href="https://pawsnclaws-petcare.herokuapp.com/" target="_blank" rel="noreferrer" className="work-box mern">
                         <h3>MERN STACK</h3>
                         <a href="https://github.com/SSPENELOPE/paws-n-claws" target="_blank" rel="noreferrer" className="abc"><FontAwesomeIcon icon={faGithub} /></a>
                     </a>
 
-                    <a href="https://codepen.io/sspenelope/full/qBxdBom" target="_blank" rel="noreferrer" className="work-box beard">
-                        <h3>Fullstack Forum/MYSQL/Handlebars</h3>
-                        <a href="https://still-beach-01787.herokuapp.com/" target="_blank" rel="noreferrer" className="abc"><FontAwesomeIcon icon={faGithub} /></a>
-                    </a>
 
-                    <a href="https://sspenelope.github.io/Weather-Dashboard/" target="_blank" rel="noreferrer" className="work-box weather">
-                        <h3>JavaScript/API</h3>
-                        <a href="https://github.com/SSPENELOPE/Weather-Dashboard" target="_blank" rel="noreferrer" className="abc"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a href="https://github.com/SSPENELOPE/ecommerce-backend" target="_blank" rel="noreferrer" className="work-box weather">
+                        <h3>MySQL/Sequelize</h3>
+                        <a href="https://github.com/SSPENELOPE/ecommerce-backend" target="_blank" rel="noreferrer" className="abc"><FontAwesomeIcon icon={faGithub} /></a>
                     </a>
  
                     <a href="https://sspenelope.github.io/Work-Day-Planner/" target="_blank" rel="noreferrer" className="work-box workday">
